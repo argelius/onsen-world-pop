49a540d4fa84a2a5501d69b72e1873c729aa4823
Andreas Argelius <andreas@argeli.us>
2016-01-20 12:56:10 +0900

    Test

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ angular.module('app', ['onsen'])
 
 .service('Population', ['$http', function($http) {
   this.get = function(country, year) {
-    return $http.jsonp('http://api.population.io:80/1.0/population/' + year + '/' + country + '/?format=jsonp&callback=JSON_CALLBACK')
+    return $http.jsonp('//api.population.io:80/1.0/population/' + year + '/' + country + '/?format=jsonp&callback=JSON_CALLBACK')
       .then(
         function(response) {
           return response.data;
